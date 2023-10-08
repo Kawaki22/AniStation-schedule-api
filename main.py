ee998c60666c69709dad42492b325005fefc775d
Prasidh Gopal Anchan <prasidhpoojary22@gmail.com>
2023-10-08 11:25:53 +0530

    Added landing page welcome message

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from scraper import Scraper
 app = FastAPI()
 scraper = Scraper()
 
+@app.get("/")
+async def read_item():
+    return "Welcome to AniStation Schedule Api 🎉"
+
 @app.get("/{season}/{category}")
 async def read_item(season, category):
     return scraper.scrapeScheduleData(season, category)
